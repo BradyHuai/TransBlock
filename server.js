e29ade8e353bdb8aa1548d22198a88c35f4dfb50
brady <bradyhuai@outlook.com>
2020-11-25 18:03:04 -0800

    Upload to heroku

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ const express = require('express')
 const app = express();
 
 const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/pub')))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+})  // localhost development port 5000  (http://localhost:5000)
+
