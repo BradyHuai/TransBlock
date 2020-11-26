9cca13914a42f96703a4f69fd59f2116fcdc9b70
brady <bradyhuai@outlook.com>
2020-11-25 17:41:55 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+/* server.js - Express server*/
+'use strict';
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
