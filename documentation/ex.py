09a3d37f2451282e6e1578f94c2adae28cf34f82
brady <bradyhuai@outlook.com>
2020-12-19 14:44:41 -0800

    Add documentation

---
+++ documentation/ex.py
@@ -0,0 +1,23 @@
+"""
+TransBlock Documentation
+-------------------------
+Author: Guo Qing Huai
+"""
+
+
+def sayHello(msg):
+    """
+    :param name: msg - Message to be shown
+    :param type: str
+    :return: str
+    """
+    return "Testing documentation"
+
+
+def sayHello2(msg):
+    """
+    :param name: msg - Message to be shown
+    :param type: str
+    :return: str
+    """
+    return "Testing documentation"
